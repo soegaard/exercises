dd45894b20a20352bf73dcb57d15c19a629a2264
Jens Axel Søgaard <jensaxel@soegaard.net>
2011-08-14 19:37:44 +0200

    Instruct git to Ignore build/*.tmp

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 whalesong/current/whalesong.rkt build stxmat-exercises.rkt
 cd build
 mv ../stxmat-exercises.xhtml .
-sed -i 'tmp' '/<body><\/body>/ r ../js-libs/load-mathjax.xhtml' stxmat-exercises.xhtml
-sed -i 'tmp' 's/<body><\/body>//' stxmat-exercises.xhtml
-sed -i 'tmp' 's/<\/html>/<\body><\/body><\/html>/' stxmat-exercises.xhtml
+sed -i '.tmp' '/<body><\/body>/ r ../js-libs/load-mathjax.xhtml' stxmat-exercises.xhtml
+sed -i '.tmp' 's/<body><\/body>//' stxmat-exercises.xhtml
+sed -i '.tmp' 's/<\/html>/<\body><\/body><\/html>/' stxmat-exercises.xhtml
 cd ..
 
